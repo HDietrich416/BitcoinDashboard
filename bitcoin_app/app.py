e905b13a4b282075273d201b3532656fd5bb1c03
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-03-16 21:46:30 -0400

    added json app routes for data

--- bitcoin_app/app.py
+++ bitcoin_app/app.py
@@ -34,6 +34,11 @@ engine = create_engine(conn, echo=False)
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
+btc_news = Base.classes.btc_news
+coin_price = Base.classes.coin_price
+market_cap = Base.classes.market_cap
+rating = Base.classes.rating
+
 session = Session(engine)
 
 ## FRONT_END ROUTES
@@ -41,6 +46,44 @@ session = Session(engine)
 def main(): 
     return render_template("index.html")
 
+## SERVICE ROUTES
+
+@app.route("/api/main/news")
+def newsRoute(): 
+    news = session.query(btc_news.Date,btc_news.Headline, btc_news.Paragraph).all()
+    news_dataset = []
+    for item in news:
+        news_dataset.append(item)
+    
+    return jsonify(news_dataset)
+
+@app.route("/api/main/price")
+def priceRoute(): 
+    price = session.query(coin_price.Date,coin_price.Open_USD, coin_price.High_USD, coin_price.Low_USD, coin_price.Close_USD).all()
+    price_dataset = []
+    for item in price:
+        price_dataset.append(item)
+    
+    return jsonify(price_dataset)
+
+@app.route("/api/main/marketcap")
+def marketcapRoute(): 
+    marketCap = session.query(market_cap.Cryptocurrencies, market_cap.Markets, market_cap.Market_Cap, market_cap.BTC_Dominance).all()
+    marcap_dataset = []
+    for item in marketCap:
+        marcap_dataset.append(item)
+    
+    return jsonify(marcap_dataset)
+
+@app.route("/api/main/rating")
+def ratingRoute(): 
+    rating_data = session.query(rating.Date, rating.Rank, rating.Score).all()
+    rating_dataset = []
+    for item in rating_data:
+        rating_dataset.append(item)
+    
+    return jsonify(rating_dataset)
 
 if __name__ == "__main__":
     app.run()
+
