86e71a442d250a282a9ef2a00e5caedee005185f
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-03-22 19:31:40 -0400

    Create config.py

---
+++ bitcoin_app/assets/config.py
@@ -0,0 +1,15 @@
+# AWS Server Details
+
+server= "bootcampdb.cmwhqarpyd9v.ca-central-1.rds.amazonaws.com"
+database = "postgres"
+port ="5432"
+username = "root"
+password = "xy92Hdie"
+
+# Alpha Vantage API Key
+
+av_api_key = 'IYOYBP274BRSLPM0'
+
+# Ney York Times API Key
+
+nyt_api_key = 'AHD58yFBu6t1DV7UGF2hdMCyfo2PvaHq'
